b209b527e9847be66d0fd7c7def7d8a57e7bcdbd
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-04 17:10:42 -0700

    Start phase 3

--- practice-for-ch-sql-movie-buff-long-practice/movie_buff/03_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice/movie_buff/03_queries.rb
@@ -1,6 +1,12 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+  Movie
+    .select("movies.title, movies.id")
+    .joins(:actors)
+    .where("actors.name IN (?)", those_actors)
+  
+    # .group("movies.id")
   
 end
 
